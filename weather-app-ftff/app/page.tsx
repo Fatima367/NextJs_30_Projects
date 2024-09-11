858b0e7b26c22419dfbd4dee21da20492828d7d5
Fatima367 <princess.fatima3275@gmail.com>
2024-09-11 19:54:32 +0500

    weather-widget-app

---
+++ weather-app-ftff/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+import WeatherWidget from "@/components/weather-widget";
+
+
+export default function Home() {
+  return (
+    <div>
+      <WeatherWidget />
+    </div>
+  );
+}
